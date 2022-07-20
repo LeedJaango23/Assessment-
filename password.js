a87d772109ec6288cbc4bf795ffdaf7873733329
MOWLID OMAR <107882442+LeedJaango23@users.noreply.github.com>
2022-07-19 22:05:35 -0500

    Add files via upload

---
+++ password.js
@@ -0,0 +1,11 @@
+let password = 'Jaango2323kjfkjghkjgkjhgl'
+// let containsNumber = /[0-9]/.test(password)
+// /\d/.test(password)
+
+//  if(password.length >= 10 && /\d/.test(password) && /[a-z0-9]/.test(password)){
+ if(password.length >= 10 && /\d/.test(password) && /[a-z0-9]/.test(password)){
+     console.log('password good')
+
+} else {
+    console.log('password no good')
+}
